fa04fa84d1053dc882dce54ea2ed3feb44f77eae
Micsta5885 <149246671+Micsta5885@users.noreply.github.com>
2024-06-16 20:16:49 +0200

    Update update-ip.sh

--- frontend/update-ip.sh
+++ frontend/update-ip.sh
@@ -5,5 +5,7 @@ IP_V4=$(curl -s $API_URL/public-ipv4)
 
 sudo sed -i "s|http://__BACKEND_IP__:3000|http://$IP_V4:3000|g" /home/ubuntu/app/frontend/nginx.conf
 sudo sed -i "s|http://localhost:3000|http://$IP_V4:3000|g" /home/ubuntu/app/frontend/index.html
+sudo sed -i "s|http://<YOUR_BACKEND_URL>|http://$IP_V4:3000|g" /home/ubuntu/app/frontend/index.html
+sudo sed -i "s|https://<YOUR_S3_BUCKET_NAME>.s3.amazonaws.com|https://tictactoe-profile-pictures-unique-name.s3.amazonaws.com|g" /home/ubuntu/app/frontend/index.html
 
-echo "Updated IP address in index.html and nginx.conf to $IP_V4"
+echo "Updated IP address and S3 bucket name in index.html and nginx.conf to $IP_V4"
